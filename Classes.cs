a9c391f436182a62b74ac6d4be86547a76d3bc77
williamhCode <83525937+williamhCode@users.noreply.github.com>
2021-09-01 17:57:14 +0800

    .

---
+++ Classes.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Text_Adventure
+{
+    
+
+
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Text_Adventure
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
         }
     }
 }
